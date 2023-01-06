a8585d9b2674b2f296445178f5d0dea849c1ab24
Joe Duong <chau.duong1995@gmail.com>
2023-01-05 23:34:15 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Header title = {1} />
+    </div>
+  );
+}
+
+export default App;
